534deea2a6c54f4fde73ef41cce11d2c9607dd1f
Calil05 <gabrielbcalil@gmail.com>
2023-05-09 17:59:15 +0000

    feat: update livro

--- app/src/main/java/application/controller/LivroController.java
+++ app/src/main/java/application/controller/LivroController.java
@@ -3,6 +3,7 @@ package application.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,6 +35,11 @@ public class LivroController {
 
         livroRepo.save(livro);
         return "redirect:/livro";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String update(@PathVariable int id) {
+        return "";
     }
 
 }
---
+++ app/src/main/webapp/WEB-INF/update.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+
+<!DOCTYPE html>
+<html lang="pt-br>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atualizar Livros</title>
+</head>
+<body>
+    <h1>Atualizar Livros</h1>
+    <form action="/insert" method="post">
+        <input type="text" name="titulo" />
+        <button type="submit">Salvar</button>
+    </form>
+</body>
+</html>
